87635cb9f15d5a1774524d45387bfc03c5aa6eb5
hameedbitlabs <99404588+hameedbitlabs@users.noreply.github.com>
2022-02-27 09:24:59 +0530

    Add files via upload

--- E1.java
+++ E1.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
 class E1{
     public static void main(String args[]){
-        int a=10,b=45;
-        int sum=a+b;
-        System.out.println("sum of "+a+" and "+b+ " is "+sum);
+        Scanner s=new Scanner(System.in);
+        int button=s.nextInt();
+        switch(button){
+            case 1: System.out.println("hi");
+            break;
+            case 2: System.out.println("hello");
+            break;
+            case 3: System.out.println("namaste");
+            break;
+            case 4: System.out.println("vanakam");
+            break;
+            
+            
+        }
     }
 }
--- E2.java
+++ E2.java
@@ -1,11 +1,26 @@
 import java.util.Scanner;
 class E2{
     public static void main(String args[]){
+        int temp=0,sum=0,rem=0;
         Scanner s=new Scanner(System.in);
-        int a =s.nextInt();
-        int b =s.nextInt();
-        int sum=0;
-        sum=a+b;
-        System.out.println("sum of "+a+" and "+b+" is "+sum);
-    }
+        System.out.println("enter number");
+        int num=s.nextInt();
+        temp=num;
+        while(num>0){
+            rem=num%10;
+            int fact=1;
+            for(int i=rem;i<rem;i++){
+                fact=fact*i;
+            }
+            num=num/10;
+            sum=sum+fact;
+        }
+        System.out.println(sum);
+        System.out.println(num);
+        if(num==sum){
+            System.out.println("strong number");
+        }else{
+            System.out.println("not strong number");
+        }
+    }    
 }
